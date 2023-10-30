943f9e3bbc53f66fd87f4b69fb0ad1f0d6e055ce
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-10-30 00:36:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+roverWidth = 100;
+roverHeight = 90;
+
+backgroundImage = "mars.jpg";
+
+roverImage = "rover.png";
+
+roverX = 10;
+roverY = 10;
+
+//continue a programar a partir daqui
